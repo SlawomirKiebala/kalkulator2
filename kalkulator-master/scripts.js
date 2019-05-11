b9a0b8b2dd3fd25882feaceeacb15d0c79dd1d03
Sławomir Kiebała <49609372+SlawomirKiebala@users.noreply.github.com>
2019-05-11 09:43:22 +0200

    Add files via upload

---
+++ kalkulator-master/scripts.js
@@ -0,0 +1,25 @@
+function calc(){}
+
+(function(){
+    function calcul(result, type){
+        this.res = document.querySelector('#' + result);
+    	this.button = document.querySelectorAll('input[type="'+type+'"]');
+    	this.calcRes();
+	}
+	
+    calcul.prototype.calcRes = function(){
+        this.button.forEach(function(item){
+            item.addEventListener('click', function(e) {
+            e.preventDefault();
+                const inputValue = item.value;
+                if(inputValue == "C") {
+                    res.value = '';
+                } else if(inputValue == "=") {
+                    res.value = eval(res.value);
+                } else {
+                    res.value += inputValue;
+                }}.bind(this))
+        });  
+	} 
+	new calcul('res', 'button');
+    })()
